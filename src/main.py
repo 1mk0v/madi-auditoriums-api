871b0aa7384c39c97407bee43167d5dc5695c456
1mk0v <potapchuk01@mail.ru>
2024-03-23 14:55:59 +0300

    init project

---
+++ src/main.py
@@ -0,0 +1,22 @@
+import config
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title='MADI Auditoriums API',
+    contact={
+        "name": "Potapchuk Danila Antonovich",
+        "url": "https://t.me/nivicki",
+        "email": "potapchuk01@mail.ru",
+    },
+)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
